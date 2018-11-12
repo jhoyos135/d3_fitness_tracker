77fed8858ffe2a38e1b3d6dd105170f96b59c99b
Julian Hoyos <jhoyos135@gmail.com>
2018-11-12 13:29:04 -0500

    first commit of fitness tracker using d3 and firebase

---
+++ index.js
@@ -0,0 +1,55 @@
+const btns = document.querySelectorAll('button');
+const form = document.querySelector('form');
+const formAct = document.querySelector('form span');
+const input = document.querySelector('input');
+const error = document.querySelector('.error');
+
+let activity = 'cycling';
+
+btns.forEach(btn => {
+
+    btn.addEventListener('click', e => {
+
+        activity = e.target.dataset.activity;
+
+        btns.forEach(btn => btn.classList.remove('active'));
+        e.target.classList.add('active');
+
+        input.setAttribute('id', activity);
+
+        formAct.textContent = activity;
+
+        update(data);
+
+    });
+
+});
+
+//form submit
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    const distance = parseInt(input.value);
+
+    if(distance) {
+        db.collection('activities').add({
+
+            distance: distance,
+            activity: activity,
+            date: new Date().toString()
+
+        }).then( () => {
+
+            error.textContent = '';
+            input.value = '';
+
+
+        })
+    } else {
+
+        error.textContent = 'please enter a valid distance';
+
+    }
+
+})
